74f6c3f014259f9f19f5afe882b104c9492d0ebf
Abdullah <abdallahmosbah25@gmail.com>
2024-04-10 00:12:35 +0200

    task100 v2

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,3 +1,7 @@
-export default function createEmployeesObject(departmentName, employees) {
-  return { [departmentName]: [...employees] };
+export default function createIteratorObject(report) {
+  const Emps = [];
+  for (const department of Object.keys(report.Emps)) {
+    Emps.push(...report.Emps[department]);
+  }
+  return Emps;
 }
