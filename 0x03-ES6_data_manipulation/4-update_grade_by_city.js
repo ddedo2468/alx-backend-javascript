058de94a1773bf34638da1e593a1eb60fe3ca360
Abdullah <abdallahmosbah25@gmail.com>
2024-04-16 02:19:39 +0200

    wtf is yajaSrift

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,12 @@
-export default function updateStudentGradeByCity(arr, city, newGrades) {
-  return arr.filter((student) => student.city === city)
-    .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      const grade = gradeObj ? gradeObj.grade : 'N/A';
-      return { ...student, grade };
-    });
+/* eslint-disable */
+export default function updateStudentGradeByCity(students, city, nGrades) {
+  return students.filter((s) => s.location === city).map((s) => {
+    const matchingGrade = nGrades.find((grade) => grade.studentId === s.id && grade.grade);
+    if (matchingGrade) {
+      s.grade = matchingGrade.grade;
+    } else {
+      s.grade = 'N/A';
+    }
+    return s;
+  });
 }
