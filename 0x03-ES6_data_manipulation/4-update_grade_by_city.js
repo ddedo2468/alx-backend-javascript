10954cbca635f1d0af6231537b1407a6c209d49a
Abdullah <abdallahmosbah25@gmail.com>
2024-04-16 02:14:35 +0200

    i really hate javaScript

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,7 +1,7 @@
 export default function updateStudentGradeByCity(arr, city, newGrades) {
   return arr.filter((item) => (item.city === city))
     .map((item) => {
-      const student = newGrades.find((item) => (item.studentId === item.id));
+      const student = newGrades.find((grade) => (grade.studentId === item.id));
       const grade = student ? student.grade : 'N/A';
       return { ...item, grade };
     });
